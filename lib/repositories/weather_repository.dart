d04f8e4d954a777042f079036f18bb6c055f3657
Khitaphat Kaeophonngam <kaeophonngam_k@silpakorn.edu>
2023-10-26 12:00:43 +0700

    update3

--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class WeatherRepository {
   Future<Map<String, dynamic>> fetchData(String city) async {
-    final apiKey = 'https://cpsu-test-api.herokuapp.com/api/1_2566/weather/current?city=bangkok';
+    final apiKey = 'http://localhost:1003';
     final endpoint = 'https://cdn.weatherapi.com/weather/128x128/day/116.png';
 
     final response = await http.get(Uri.parse('$endpoint?q=$city&apiKey=$apiKey'));
